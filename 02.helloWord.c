e8b4ca1988399251f05fdbfda7d1a74df37b032d
Kewen <kevinwen2014@gmail.com>
2023-03-10 18:30:07 +0800

    reupload 01. upload 02.

---
+++ 02.helloWord.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    printf("H           H         A\n"
+           "H           H        A A\n"
+           "H           H       A   A\n"
+           "HHHHHHHHHHHHH      AAAAAAA\n"
+           "H           H     A       A\n"
+           "H           H    A         A\n"
+           "H           H   A           A\n"
+    );
+return 0;
+}
