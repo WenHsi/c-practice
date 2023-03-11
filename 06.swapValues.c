a11b278287d1941b71a4826f22180afc36210d67
root <root@instance-1.asia-east1-b.c.main-379422.internal>
2023-03-11 13:22:59 +0000

    create 06.swapValues

---
+++ 06.swapValues.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int integer1, integer2, temp;
+    printf("Please enter the first integer to be swapped and viewed: ");
+    scanf("%d", &integer1);
+    printf("Please enter the second integer to be swapped and viewed: ");
+    scanf("%d", &integer2);
+    temp=integer1;
+    integer1=integer2;
+    integer2=temp;
+    printf("integer1: %d.\n", integer1);
+    printf("integer2: %d.\n", integer2);
+    return 0;
+}
