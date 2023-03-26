5d2e4a2ce4ee56593e44dc2ca2a2aa2fb6f1ecf1
Kewen <kevinwen2014@gmail.com>
2023-03-26 09:55:30 +0000

    修改後第二版

--- 10.VolumeDiscount.c
+++ 10.VolumeDiscount.c
@@ -7,11 +7,11 @@ int main() {
   scanf("%d", &count);
   int total = cost * count;
   if (total >= 3000) {
-    double sale = total * 0.8;
-    int saleTypeToInt = sale;
-    printf("總金額為（已打8折）：%d\n", saleTypeToInt);
+    total = total * 0.8;
+    printf("總金額為（已打8折）：%d\n", total);
   } else {
-    printf("總金額為： %d\n", total);
+    // total = total;
+    printf("總金額為：%d\n ", total);
   }
   return 0;
 }
