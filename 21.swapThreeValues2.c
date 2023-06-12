6df1c3d16851f3fb1b208fdab3a22e95bef88ad7
Kewen <kevinwen2014@gmail.com>
2023-06-12 05:39:55 +0000

    換成從右至左交換

--- 21.swapThreeValues2.c
+++ 21.swapThreeValues2.c
@@ -4,21 +4,38 @@ int main() {
   printf("Please enter three integers: ");
   scanf("%d%d%d", &a, &b, &c);
   printf("Before: %d %d %d\n", a, b, c);
-  if (b < a) {
-    t = b;
-    b = a;
-    a = t;
+  //從右換到左
+  if (c < b) {
+    t = c;
+    c = b;
+    b = t;
   }
   if (c < a) {
     t = c;
     c = a;
     a = t;
   }
-  if (c < b) {
-    t = c;
-    c = b;
-    b = t;
+  if (b < a) {
+    t = b;
+    b = a;
+    a = t;
   }
+  //從左換到右
+  // if (b < a) {
+  //   t = b;
+  //   b = a;
+  //   a = t;
+  // }
+  // if (c < a) {
+  //   t = c;
+  //   c = a;
+  //   a = t;
+  // }
+  // if (c < b) {
+  //   t = c;
+  //   c = b;
+  //   b = t;
+  // }
   printf("After: %d %d %d\n", a, b, c);
   return 0;
 }
