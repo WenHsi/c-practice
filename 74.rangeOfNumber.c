eaac43bf62962e92a7f3d57dd10578b9ce8ffc15
Kewen <kevinwen2014@gmail.com>
2023-12-31 15:55:18 +0000

    Add new progress

---
+++ 74.rangeOfNumber.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+  int i, j, n, b[10] = {0};
+  printf("Please enter ten integer: ");
+  for (i = 1; i <= 10; i++) {
+    scanf("%d", &n);
+    b[(n - 1) / 10]++;
+  }
+  for (i = 1; i <= 10; i++) {
+    printf("%3d: ", i * 10);
+    for (j = 1; j <= b[i - 1]; j++) {
+      printf("*");
+    }
+    printf("\n");
+  }
+  return 0;
+}
